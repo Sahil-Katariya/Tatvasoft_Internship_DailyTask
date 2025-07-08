ef90475920bb9de7532fc5aef13e6d925a97a9ad
SAHIL KATARIYA <Sahil-Katariya@users.noreply.github.com>
2025-07-08 16:45:40 +0530

    Day 3 updated

--- Day2/GEC/src/app/app.component.ts
+++ Day2/GEC/src/app/app.component.ts
@@ -1,38 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
+import { RouterOutlet } from "@angular/router";
+import { NgToastModule } from "ng-angular-popup";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,RouterOutlet,CommonModule,FormsModule],
+  imports: [RouterOutlet, NgToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-// export class AppComponent {
-//   title = 'gec';
-//   name = 'Gec Bvn';
-//   visibility = true;
-//   list = [12,23,34,45,56];
-
-// store = " "
-//  listner(){
-//    this.visibility = !this.visibility;
-//    console.log(this.store)
-//  }
-// }
-
 export class AppComponent {
-  title = 'Login';
-  username = '';
-  pass = '';
-
-  loginCheck() {
-    if (this.username === 'test' && this.pass === '123') {
-      console.log("Login Successful");
-    } else {
-      console.log("Login Failed");}
-  }
+  title = 'FrontEnd';
 }
