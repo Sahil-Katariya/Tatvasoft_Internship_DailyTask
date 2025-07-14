0a9ce7a1569198bcf9fa2a529b5e7daaffd02cc9
SAHIL KATARIYA <Sahil-Katariya@users.noreply.github.com>
2025-07-14 15:22:12 +0530

    day 8 task updated

---
+++ Day6/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<MissionSkill> MissionSkills { get; set; }
+
+        public DbSet<MissionTheme> MissionThemes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Sahil",
+                LastName = "katariya",
+                EmailAddress = "sahil@gmail.com",
+                Password = "sahil",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
